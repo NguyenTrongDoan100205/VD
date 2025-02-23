613440a245e2fae93edf5e60ebdf166c73019946
NguyenTrongDoan100205 <nguyentrongdoanns@iCloud.com>
2025-02-23 11:10:39 +0700

    Add project files.

---
+++ VD/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using VD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using VD.Data; // Chứa StudentContext
+
+namespace VD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentContext _context;
+
+        public StudentsController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        //action
+        [HttpPost("CreateStudent")]
+        public async Task<IActionResult> CreateStudent(Student student)
+        {
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Student added successfully", student });
+
+        }
+
+    }
+}
